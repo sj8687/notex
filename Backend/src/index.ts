3663d71525bd7c3953c7178337a16bb0305b2650
sj8687 <shreyashjadhav59807@gmail.com>
2025-03-19 20:08:40 +0530

    add

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import { loginRoute } from "./routes/loginRoute";
 import cors from "cors"
-import cookieParser from "cookie-parser";
+const cookieParser = require("cookie-parser");
 import * as dotenv from 'dotenv';
 import { notesRoute } from "./routes/notesRoute";
 import { contactRoutes } from "./routes/contactRoute";
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
   origin:process.env.FRONTEND_URL,
   credentials: true
